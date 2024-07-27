db8aa974d70febe0d9d9d877896bfd7ba40f94fb
Irina Dobrenco <irika.22181@gmail.com>
2024-07-27 13:33:33 +0300

    Initial commit

---
+++ src/js/modules/food.js
@@ -0,0 +1,35 @@
+import {NumberUtils} from "../utils/number-utils.js";
+
+export class Food {
+    foodRadius = null;
+    context = null;
+    positionsCount = null;
+    positionsSize = null;
+    foodPosition = {
+        x: 1,
+        y: 1
+    }
+    constructor(context, positionsCount, positionsSize) {
+        this.context = context;
+        this.positionsCount = positionsCount;
+        this.positionsSize = positionsSize;
+
+        this.foodRadius = this.positionsSize / 2;
+    }
+
+    setNewFoodPosition() {
+        this.foodPosition = {
+            x: NumberUtils.getRandomInit(1, this.positionsCount),
+            y: NumberUtils.getRandomInit(1, this.positionsCount),
+        }
+    }
+
+    showFood() {
+        this.context.fillStyle = 'white';
+        this.context.beginPath();
+        this.context.arc(this.foodPosition.x * this.positionsSize - this.foodRadius,
+            this.foodPosition.y + this.positionsSize - this.foodRadius, this.foodRadius, 0, 2 + Math.PI);
+        this.context.fill();
+
+    }
+}
